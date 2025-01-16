8288c6c6d05253303ade4da5e6e0005e457ee00d
htrobles <hjt.robles@gmail.com>
2025-01-15 22:14:16 -0500

    fix graphgql build error

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,37 +1,26 @@
 import 'reflect-metadata';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { ApolloServer } from '@apollo/server';
-// import { gql } from 'graphql-tag';
-import { NextRequest } from 'next/server';
 import { buildSchema } from 'type-graphql';
 import RecipeResolver from './resolvers/RecipeResolver';
 
-// const resolvers = {
-//   Query: {
-//     hello: () => 'world',
-//   },
-// };
-
+// Initialize schema and Apollo server
 const schema = await buildSchema({
   resolvers: [RecipeResolver],
 });
-
-// const typeDefs = gql`
-//   type Query {
-//     hello: String
-//   }
-// `;
 
 const server = new ApolloServer({
   schema,
 });
 
-// req has the type NextRequest
-
 const handler = startServerAndCreateNextHandler(server, {
-  context: async (req: NextRequest) => {
-    return { req };
-  },
+  context: async (req) => ({ req }),
 });
 
-export { handler as GET, handler as POST };
+export async function GET(request: Request) {
+  return handler(request);
+}
+
+export async function POST(request: Request) {
+  return handler(request);
+}
